536ed007dfd414573d3327755e115f8d567c9e6b
Kamilgeek <kamil-magomedov-00@mail.ru>
2019-11-07 12:39:12 +0400

    Did Server and client

--- lesson_1/task_2.py
+++
@@ -1,8 +0,0 @@
-
-wrds = [b'class', b'function', b'method']
-
-for i in wrds:
-    print(f'тип переменной: {type(i)}')
-    print(f'содержимое переменной: {i}')
-    print(f'длинна строки: {len(i)}')
-
