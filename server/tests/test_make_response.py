23fd795cd9ef099827aed4f1a0db7a43e0c66288
Kamilgeek <kamil-magomedov-00@mail.ru>
2019-11-11 15:31:22 +0400

    did test make_response function with ficture

--- server/tests/test_make_response.py
+++ server/tests/test_make_response.py
@@ -1,21 +1,53 @@
+import pytest
 from datetime import datetime
 from protocol import make_response
 
-TIME = datetime.now().timestamp()
-CODE = 200
 
-REQUEST = {
-    'action': 'test',
-    'time': TIME,
-    'data': 'message',
-}
+@pytest.fixture
+def action_fixture():
+    return 'test_action'
 
-RESPONSE = {
-    'action': 'test',
-    'time': TIME,
-    'data': 'message',
-    'code': 200
-}
+
+@pytest.fixture
+def time_fixture():
+    return datetime.now().timestamp()
+
+
+@pytest.fixture
+def data_fixture():
+    return 'message'
+
+
+@pytest.fixture
+def code_fixture():
+    return 200
+
+
+@pytest.fixture
+def response_fixture(action_fixture, time_fixture, data_fixture, code_fixture):
+    return {
+        'action': action_fixture,
+        'time': time_fixture,
+        'data': data_fixture,
+        'code': code_fixture
+    }
+
+
+# TIME = datetime.now().timestamp()
+# CODE = 200
+#
+# REQUEST = {
+#     'action': 'test',
+#     'time': TIME,
+#     'data': 'message',
+# }
+#
+# RESPONSE = {
+#     'action': 'test',
+#     'time': TIME,
+#     'data': 'message',
+#     'code': 200
+# }
 
 
 def test_valid_make_response():
