27525cc498953b77c5e63e8aa1a0ac538c2db71f
Kamilgeek <kamil-magomedov-00@mail.ru>
2019-11-16 12:56:41 +0400

    example with @ but without parametrs

--- class_work/lesson_6/decorators.py
+++ class_work/lesson_6/decorators.py
@@ -1,13 +1,31 @@
-def sample(text, num):
-    return text * num
-
+# def sample(text, num):
+#     return text * num
+#
 def logged(func):
     def wrapper(text, num):
         result = func(text, num)
         print(f'log - {func.__name__}({text, num}) = {result} ')
         return result
     return wrapper
+#
+# sample_clone = logged(sample)
+# sample_clone('text', 3)
+#
+# @logged
+# def sample(text, num):
+#      return text * num
 
-sample_clone = logged(sample)
-sample_clone('text', 3)
+# sample('text', 3)
 
+def delimited(func):
+    def wrapper(*args, **kwargs):
+        print('*' * 50)
+        return func(*args, **kwargs)
+    return wrapper
+
+@logged
+@delimited
+def sample(text, num):
+     return text * num
+
+sample('text', 3)
