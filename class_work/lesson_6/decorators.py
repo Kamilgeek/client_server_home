d6291e221ab364d24a12af0157244ca8123d15f2
Kamilgeek <kamil-magomedov-00@mail.ru>
2019-11-17 10:16:21 +0400

    example without @ but with parametrs

--- class_work/lesson_6/decorators.py
+++ class_work/lesson_6/decorators.py
@@ -1,12 +1,12 @@
 # def sample(text, num):
 #     return text * num
 #
-def logged(func):
-    def wrapper(text, num):
-        result = func(text, num)
-        print(f'log - {func.__name__}({text, num}) = {result} ')
-        return result
-    return wrapper
+# def logged(func):
+#     def wrapper(text, num):
+#         result = func(text, num)
+#         print(f'log - {func.__name__}({text, num}) = {result} ')
+#         return result
+#     return wrapper
 #
 # sample_clone = logged(sample)
 # sample_clone('text', 3)
@@ -23,9 +23,28 @@ def delimited(func):
         return func(*args, **kwargs)
     return wrapper
 
-@logged
-@delimited
-def sample(text, num):
-     return text * num
+# @logged
+# @delimited
+# def sample(text, num):
+#      return text * num
 
-sample('text', 3)
+# sample('text', 3)
+
+def logged(log_format):
+    def decorator(func):
+        def wrapper(text, num):
+            result = func(text, num)
+            name = func.__name__
+            args = ','.join((text, str(num)))
+            print(log_format % {'name': name, 'args': args, 'result': result})
+            return result
+        return wrapper
+    return decorator
+
+
+def sample(text, num):
+    return text * num
+
+decorator = logged('%(name)s(%(args)s) = %(result)s')
+sample_clone = decorator(sample)
+sample_clone('sample', 2)
