6fb97f1c4939ccb61b4beaeba24734c26277cd0e
Kamilgeek <kamil-magomedov-00@mail.ru>
2019-11-21 09:49:49 +0400

    did handler function

--- server/__main__.py
+++ server/__main__.py
@@ -3,6 +3,7 @@ import json
 import logging
 from socket import socket
 from argparse import ArgumentParser
+from handlers import handle_default_request
 from resolvers import resolve
 from protocol import validate_request, make_response
 
@@ -49,28 +50,10 @@ try:
         client, addres = sock.accept()
         logging.info(f'client was connected with {addres[0]}:{addres[1]}')
         b_request = client.recv(defaul_config.get('buffersize'))
-        request = json.loads(b_request.decode())
-
-        if validate_request(request):
-            action_name = request.get('action')
-            controller = resolve(action_name)
-
-            if controller:
-                try:
-                    logging.debug(f'Controller {action_name} resolved with request{request}')
-                    response = controller(request)
-                except Exception as err:
-                    logging.critical(f'Controller {action_name} error: {err}')
-                    response = make_response(request, 500, 'Internal server error')
-            else:
-                logging.error(f'Controller {action_name} not found')
-                response = make_response(request, 404, f'Action with name {action_name} not supported')
-        else:
-            logging.error(f'Controller wrong request {request}')
-            response = make_response(request, 400, 'wrong request format')
-
+        b_response = handle_default_request(b_request)
+        
         client.send(
-            json.dumps(response).encode()
+            b_response
         )
 
         client.close()
